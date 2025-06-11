30728b868b81499ed9942cb6f742cba0c5d737f5
juanmartinmn <juan.molinanavarro@alu.frt.utn.edu.ar>
2025-06-11 17:52:54 -0300

    Avances en el header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import cgLogo from '../../../public/compra-gamer.svg'
 import '../../styles/Header.css'
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa6";
+
 
 const Header = () => {
     return (
         <header>
             <img src={cgLogo} className="logo" alt="compra gamer logo" />
-            <button></button>
-            <button></button>
+            <input type="text" placeholder='Buscar productos' />
+            <button className='busquedaHeader'>
+                <FaRegUser color='#f0320a'/>
+                Ingresá</button>
+            <button><MdOutlineShoppingCart color='#f0320a' />
+            </button>
         </header>
     )
 }
