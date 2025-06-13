17726ada4d5847287863d58960bb2b24998ecc8a
juanmartinmn <juan.molinanavarro@alu.frt.utn.edu.ar>
2025-06-13 12:33:05 -0300

    Estilos de header terminado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,26 @@ import cgLogo from '../../../public/compra-gamer.svg'
 import '../../styles/Header.css'
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa6";
+import { IoIosSearch } from "react-icons/io";
 
 
 const Header = () => {
     return (
         <header>
             <img src={cgLogo} className="logo" alt="compra gamer logo" />
-            <input type="text" placeholder='Buscar productos' />
-            <button className='busquedaHeader'>
+            <div className='searchBar'>
+                <input id="searchInput" type="text" required placeholder='Buscar productos' />
+                <IoIosSearch className='searchIcon' color='f0320a'/>
+            </div>
+            <div id='buttonContainer'>
+            <button>
                 <FaRegUser color='#f0320a' /> Ingresá
             </button>
             <button>
                 <MdOutlineShoppingCart color='#f0320a' />
             </button>
+
+            </div>
         </header>
     )
 }
