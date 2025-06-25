f406aed385805cd81412855f1a8512798aa206f7
juanmartinmn <juan.molinanavarro@alu.frt.utn.edu.ar>
2025-06-25 13:39:05 -0300

    agregado h y f a productos categoria

--- src/pages/ProductosCategoria.jsx
+++ src/pages/ProductosCategoria.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import MainProductosCategoria from '../components/Main/MainProductos/MainProductosCategoria'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
 
 const ProductosCategoria = () => {
-  return (
-    <div>
-        <MainProductosCategoria></MainProductosCategoria>
-
-    </div>
-  )
+    return (
+        <div>
+            <Header></Header>
+            <MainProductosCategoria></MainProductosCategoria>
+            <Footer></Footer>
+        </div>
+    )
 }
 
 export default ProductosCategoria
